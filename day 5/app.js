1c4386fbe787b604a7a4c27cb30db25cbe3cdb75
vladimer <gshatakishvili@gmail.com>
2021-08-17 22:27:09 +0400

    Practice promises

---
+++ day 5/app.js
@@ -0,0 +1,65 @@
+
+function request(url) {
+    return new Promise(accept => {
+        // gavushvi request 
+        // movida monacemebi 
+        // 
+        accept([1, 2, 3])
+    });
+}
+
+async function studentebisdaunivesttwamogeba() {
+    const students = t wamoigeStudneteb()
+    wamoigeSangebbi(students);
+}
+
+function wamoigeUniversitetebi() {
+    // 
+
+    mysql 
+
+    backedn  c# // 127.0.0.1:300
+    // studnteb(name) {
+        
+    }
+
+    frontend js // 127.0.0.1:300/studentebi?name=leo
+}
+
+function main() {
+    await studentebisdaunivesttwamogeba()
+    wamoigeUniveawairsitetebi();
+}
+
+async function motana() {
+    // riqvesti studentebze 
+    // sagnebis amogeba 
+
+    const students = moitaneStudenbe()
+        .then(studentebi => {
+            wamoigeSangebbi(studentebi);
+        })
+
+    wamoigeUniversitetebi();
+
+    // universitetebi
+
+    // sinqronuli 
+    const data = await request('https:3000/get-data')
+    console.log(data);
+    
+    // asinqronuli
+    const otherData = request('...')
+        .then(masivi => {
+            console.log("asinqronuli data")
+            console.log(masivi);
+        })
+
+    console.log("morcha yvelaferi");
+    // return data;
+}
+
+// function daxatva(masivi) {}
+
+const result = motana();
+// daxatva(result);

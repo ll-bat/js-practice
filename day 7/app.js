f0e5515887bd3f22855a55c64da9ead85a63edf5
vladimer <gshatakishvili@gmail.com>
2021-08-21 00:06:13 +0400

    init day 7

---
+++ day 7/app.js
@@ -0,0 +1,34 @@
+
+// GET, POST, PUT, PATCH, DELETE
+
+
+// const url = "https://jsonplaceholder.typicode.com/users";
+// fetch(url, {
+//     method: "POST",
+//     body: JSON.stringify({
+//         name: "leo",
+//         surname: "bzishvili"
+//     })
+// })
+
+// fetch(url + "?name=leo&surname=bzishvili&age=32")
+
+let someDiv = document.createElement('div');
+
+let div = $(someDiv)
+        .addClass('rame')
+        .removeClass('svxva-rame')
+        .css({ background: 'blue' })
+
+// $.ajax({
+//     method: "POST",
+//     url: "rame",
+//     data: {
+//         name: "leo"
+//     },
+//     success: (res) => {
+
+//     }
+// })
+
+console.log(div);
